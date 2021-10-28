c9b5ebc6f87b4bafd8e83838adc11942876755dd
rich1e <yuqigong@outlook.com>
2021-10-28 19:05:19 +0800

    add 添加ESLint & 修改.editorconfig

---
+++ packages/upload-client/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  // 此项是用来告诉eslint找当前配置文件不能往父级查找
+  root: true,
+  // 全局环境
+  env: {
+    node: true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    // 指定ESlint的解析器
+    parser: '@typescript-eslint/parser',
+    // 允许使用ES语法
+    ecmaVersion: 'latest',
+    // 允许使用import
+    sourceType: 'module',
+    // 允许解析JSX
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+		'@typescript-eslint/no-explicit-any': 'off',
+    'vue/script-setup-uses-vars': 'error',
+    'vue/no-v-html': 'off',
+    'vue/comment-directive': 'off'
+  }
+}
